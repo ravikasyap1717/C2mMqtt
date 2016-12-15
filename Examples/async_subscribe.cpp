58f9a7d2d28d9bde1732d500d4b117b23ac5c3f2
ravikasyap1717 <ravikasyap1717@yahoo.com>
2016-12-15 15:29:56 +0530

    C2M Mqtt Example

---
+++ Examples/async_subscribe.cpp
@@ -0,0 +1,174 @@
+/*******************************************************************************
+ * Copyright (c) 2013-2016 Frank Pagliughi <fpagliughi@mindspring.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Frank Pagliughi - initial implementation and documentation
+ *******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <thread>
+#include <chrono>
+#include "mqtt/async_client.h"
+
+//const std::string ADDRESS("tcp://localhost:1883");
+const std::string ADDRESS("199.108.99.17:5001");
+const std::string CLIENTID("AsyncSubcriber");
+const std::string TOPIC("hello");
+
+const int  QOS = 1;
+const long TIMEOUT = 10000L;
+
+/////////////////////////////////////////////////////////////////////////////
+
+class action_listener : public virtual mqtt::iaction_listener
+{
+	std::string name_;
+
+	virtual void on_failure(const mqtt::itoken& tok) {
+		std::cout << name_ << " failure";
+		if (tok.get_message_id() != 0)
+			std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
+		std::cout << std::endl;
+	}
+
+	virtual void on_success(const mqtt::itoken& tok) {
+		std::cout << name_ << " success";
+		if (tok.get_message_id() != 0)
+			std::cout << " for token: [" << tok.get_message_id() << "]" << std::endl;
+		if (!tok.get_topics().empty())
+			std::cout << "\ttoken topic: '" << tok.get_topics()[0] << "', ..." << std::endl;
+		std::cout << std::endl;
+	}
+
+public:
+	action_listener(const std::string& name) : name_(name) {}
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Local callback & listener class for use with the client connection.
+ * This is primarily intended to receive messages, but it will also monitor
+ * the connection to the broker. If the connection is lost, it will attempt
+ * to restore the connection and re-subscribe to the topic.
+ */
+class callback : public virtual mqtt::callback,
+					public virtual mqtt::iaction_listener
+
+{
+	int nretry_;
+	mqtt::async_client& cli_;
+	mqtt::connect_options& connOpts_;
+	action_listener subListener_;
+
+	void reconnect() {
+		std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+		try {
+			cli_.connect(connOpts_, nullptr, *this);
+		}
+		catch (const mqtt::exception& exc) {
+			std::cerr << "Error: " << exc.what() << std::endl;
+			exit(1);
+		}
+	}
+
+	// Re-connection failure
+	virtual void on_failure(const mqtt::itoken& tok) {
+		std::cout << "Connection failed" << std::endl;
+		if (++nretry_ > 5)
+			exit(1);
+		reconnect();
+	}
+
+	// Re-connection success
+	virtual void on_success(const mqtt::itoken& tok) {
+		std::cout << "\nConnection success" << std::endl;
+		std::cout << "\nSubscribing to topic '" << TOPIC << "'\n"
+			<< "\tfor client " << CLIENTID
+			<< " using QoS" << QOS << "\n"
+			<< "Press Q<Enter> to quit\n" << std::endl;
+
+		cli_.subscribe(TOPIC, QOS, nullptr, subListener_);
+	}
+
+	virtual void connection_lost(const std::string& cause) {
+		std::cout << "\nConnection lost" << std::endl;
+		if (!cause.empty())
+			std::cout << "\tcause: " << cause << std::endl;
+
+		std::cout << "Reconnecting..." << std::endl;
+		nretry_ = 0;
+		reconnect();
+	}
+
+	virtual void message_arrived(const std::string& topic, mqtt::const_message_ptr msg) {
+		std::cout << "Message arrived" << std::endl;
+		std::cout << "\ttopic: '" << topic << "'" << std::endl;
+		std::cout << "\t'" << msg->to_str() << "'\n" << std::endl;
+	}
+
+	virtual void delivery_complete(mqtt::idelivery_token_ptr token) {}
+
+public:
+	callback(mqtt::async_client& cli, mqtt::connect_options& connOpts)
+				: nretry_(0), cli_(cli), connOpts_(connOpts), subListener_("Subscription") {}
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+	mqtt::connect_options connOpts;
+	connOpts.set_keep_alive_interval(20);
+	connOpts.set_clean_session(true);
+
+	mqtt::async_client client(ADDRESS, CLIENTID);
+
+	callback cb(client, connOpts);
+	client.set_callback(cb);
+
+	// Start the connection.
+	// When completed, the callback will subscribe to topic.
+
+	try {
+		std::cout << "Connecting to the MQTT server..." << std::flush;
+		client.connect(connOpts, nullptr, cb);
+	}
+	catch (const mqtt::exception& exc) {
+		std::cerr << "\nERROR: Unable to connect to MQTT server: " << ADDRESS << std::endl;
+		return 1;
+	}
+
+	// Just block till user tells us to quit.
+
+	while (std::tolower(std::cin.get()) != 'q')
+		;
+
+	// Disconnect
+
+	try {
+		std::cout << "\nDisconnecting from the MQTT server..." << std::flush;
+		client.disconnect()->wait_for_completion();
+		std::cout << "OK" << std::endl;
+	}
+	catch (const mqtt::exception& exc) {
+		std::cerr << "Error: " << exc.what() << std::endl;
+		return 1;
+	}
+
+ 	return 0;
+}
+
